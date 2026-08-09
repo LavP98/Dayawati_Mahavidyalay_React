e9dabda23816d3f3782799c70da58d142bc59643
Lav Pandey <lavpandey63@gmail.com>
2026-08-09 19:23:16 +0530

    menu fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { navItems } from '../data/siteData'
+import { useState } from 'react';
+import { navItems } from '../data/siteData';
 
 interface NavbarProps {
-  isMobile: boolean
-  onNavClick: (navItem: string) => void
+  isMobile: boolean;
+  onNavClick: (navItem: string) => void;
 }
 
 export default function Navbar({ isMobile, onNavClick }: NavbarProps) {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleClick = (navItem: string) => {
-    setMenuOpen(false)
-    onNavClick(navItem)
-  }
+    setMenuOpen(false);
+    onNavClick(navItem);
+  };
 
   if (isMobile) {
     return (
@@ -24,31 +24,39 @@ export default function Navbar({ isMobile, onNavClick }: NavbarProps) {
           aria-expanded={menuOpen}
           aria-label="Toggle navigation menu"
         >
+          {/* <span />
           <span />
-          <span />
-          <span />
+          <span /> */}
           Menu
         </button>
         {menuOpen && (
           <div className="mobile-menu">
             {navItems.map((item) => (
-              <button key={item.navItem} type="button" onClick={() => handleClick(item.navItem)}>
+              <button
+                key={item.navItem}
+                type="button"
+                onClick={() => handleClick(item.navItem)}
+              >
                 {item.name}
               </button>
             ))}
           </div>
         )}
       </nav>
-    )
+    );
   }
 
   return (
     <nav className="navbar desktop-nav">
       {navItems.map((item) => (
-        <button key={item.navItem} type="button" onClick={() => handleClick(item.navItem)}>
+        <button
+          key={item.navItem}
+          type="button"
+          onClick={() => handleClick(item.navItem)}
+        >
           {item.name}
         </button>
       ))}
     </nav>
-  )
+  );
 }
